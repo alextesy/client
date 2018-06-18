491e752d3cfc5ba384cfdc845f95ff6e4e965467
idanrazy <idan.razy@gmail.com>
2018-06-18 20:49:56 +0300

    cc

--- components/favoritesController.js
+++ components/favoritesController.js
@@ -1,5 +1,5 @@
 angular.module("poiApp")
-    .controller('favoritesController', ['$location','$scope','$http','localStorageModel','getlocalpois','dbpois',function($location,$scope,$http,localStorageModel,getlocalpois,dbpois) {
+    .controller('favoritesController', ['$location','$scope','$http','localStorageModel','getlocalpois','dbpois','localdeletepois',function($location,$scope,$http,localStorageModel,getlocalpois,dbpois,localdeletepois) {
         let serverUrl='http://localhost:3000/'
         self=this;
         $scope.poiorder=[];
@@ -18,6 +18,7 @@ angular.module("poiApp")
 
        $scope.checkIfExists=function(arr,poi){
             for(var i=0;i<arr.length;i++){
+                console.log(arr[i].ID);
                 if(arr[i].ID==poi.ID){
                     return true;
                 }
@@ -25,52 +26,50 @@ angular.module("poiApp")
             return false;
         }
 
-
-        $scope.loadpois(){
-                    //         data = response.data;
-        //         $scope.poiarray ={};
-        //         var promiseArr=[];
-        }
-        //     $http.get(serverUrl+'users/log/saved')
-        //     .then(function(response){
-        //         data = response.data;
-        //         $scope.poiarray ={};
-        //         var promiseArr=[];
-        //     for(var i =0;i<data.length;i++){
-        //         promiseArr.push($http.get(serverUrl+'POI/'+data[i].poiID))             
-        //     }
-        //     Promise.all(promiseArr)
-        //     .then(function(result){
-        //         for(var i=0;i<result.length;i++){
-        //             var poi={};
-        //             poi=result[i].data.poidetails[0];
-        //             poi.image=result[i].data.images[0].image;
-        //             poi.reviews=result[i].reviews;
-        //             $scope.poiarray[poi.ID]=poi;
-        //             var Dbpois=dbpois.get_dbpois();
-        //             if(Dbpois){
-        //                 if(!$scope.checkIfExists(Dbpois,poi))
-        //                 dbpois.update_dbpois(poi);
-        //             }
-        //             else{
-        //                 dbpois.update_dbpois(poi);
-        //             }
+        $http.get(serverUrl+'users/log/saved')
+            .then(function(response){
+                data = response.data;
+                $scope.poiarray ={};
+                var promiseArr=[];
+            for(var i =0;i<data.length;i++){
+                promiseArr.push($http.get(serverUrl+'POI/'+data[i].poiID))             
+            }
+            Promise.all(promiseArr)
+            .then(function(result){
+                var deletpois = localdeletepois.get_local_deletepois(); 
+                if(deletpois=="undefined")
+                    deletpois = [];
+                for(var i=0;i<result.length;i++){
+                    var poi={};
+                    poi=result[i].data.poidetails[0];
+                    poi.image=result[i].data.images[0].image;
+                    poi.reviews=result[i].reviews;
+                    if(!$scope.checkIfExists(deletpois,poi.ID))
+                        $scope.poiarray[poi.ID]=poi;
+                    var Dbpois=dbpois.get_dbpois();
+                    if(Dbpois){
+                        if(!$scope.checkIfExists(Dbpois,poi))
+                        dbpois.update_dbpois(poi);
+                    }
+                    else{
+                        dbpois.update_dbpois(poi);
+                    }
                    
-        //         }
-        //         localPoi=getlocalpois.get_local_pois();
-        //         if(localPoi){
-        //             for(var i=0;i<localPoi.length;i++){
-        //                 $scope.poiarray[localPoi[i].ID]=localPoi[i];
-        //             }
-        //         }
-        //         $scope.showimgs = true;
-        //         $scope.poiorder=Object.keys($scope.poiarray);
-        //         $scope.$apply();
+                }
+                localPoi=getlocalpois.get_local_pois();
+                if(localPoi){
+                    for(var i=0;i<localPoi.length;i++){
+                        $scope.poiarray[localPoi[i].ID]=localPoi[i];
+                    }
+                }
+                $scope.showimgs = true;
+                $scope.poiorder=Object.keys($scope.poiarray);
+                $scope.$apply();
 
-        //     })
-        // },function(response){
-        //     alert("something went wrong");
-        // })
+            })
+        },function(response){
+            alert("something went wrong");
+        })
         
         
         $scope.updatepostion_up = function(index){
