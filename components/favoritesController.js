9fcaf83fb82c6808fc6b78aea583cf4e6e2e0a59
idanrazy <idan.razy@gmail.com>
2018-06-18 16:59:51 +0300

    commit

--- components/favoritesController.js
+++ components/favoritesController.js
@@ -4,16 +4,18 @@ angular.module("poiApp")
         self=this;
         $scope.poiorder=[];
         
-        /*
-        $scope.$on('addpoi',function(response,oArgs){
-            $scope.login = $rootScope.login;
-            $http.get(serverUrl+'POI/'+oArgs)
-            .then(function(response){
-              $scope.poiarray.push(response.data)
-               
-            })
-        });
-        */
+        
+        var mymap = L.map('mapid').setView([40.782281, -73.969151], 13);
+        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.streets',
+        accessToken: 'pk.eyJ1IjoicmF6eWlkIiwiYSI6ImNqaWtiOGUzazF3aWYza3FwejVsa2d4ZnoifQ.e20DpGD798sL470V93XvQg'
+        }).addTo(mymap);
+
+
+
+
        $scope.checkIfExists=function(arr,poi){
             for(var i=0;i<arr.length;i++){
                 if(arr[i].ID==poi.ID){
@@ -23,46 +25,52 @@ angular.module("poiApp")
             return false;
         }
 
-            $http.get(serverUrl+'users/log/saved')
-            .then(function(response){
-                data = response.data;
-                $scope.poiarray ={};
-                var promiseArr=[];
-            for(var i =0;i<data.length;i++){
-                promiseArr.push($http.get(serverUrl+'POI/'+data[i].poiID))             
-            }
-            Promise.all(promiseArr)
-            .then(function(result){
-                for(var i=0;i<result.length;i++){
-                    var poi={};
-                    poi=result[i].data.poidetails[0];
-                    poi.image=result[i].data.images[0].image;
-                    poi.reviews=result[i].reviews;
-                    $scope.poiarray[poi.ID]=poi;
-                    var Dbpois=dbpois.get_dbpois();
-                    if(Dbpois){
-                        if(!$scope.checkIfExists(Dbpois,poi))
-                        dbpois.update_dbpois(poi);
-                    }
-                    else{
-                        dbpois.update_dbpois(poi);
-                    }
-                   
-                }
-                localPoi=getlocalpois.get_local_pois();
-                if(localPoi){
-                    for(var i=0;i<localPoi.length;i++){
-                        $scope.poiarray[localPoi[i].ID]=localPoi[i];
-                    }
-                }
-                $scope.showimgs = true;
-                $scope.poiorder=Object.keys($scope.poiarray);
-                $scope.$apply();
 
-            })
-        },function(response){
-            alert("something went wrong");
-        })
+        $scope.loadpois(){
+                    //         data = response.data;
+        //         $scope.poiarray ={};
+        //         var promiseArr=[];
+        }
+        //     $http.get(serverUrl+'users/log/saved')
+        //     .then(function(response){
+        //         data = response.data;
+        //         $scope.poiarray ={};
+        //         var promiseArr=[];
+        //     for(var i =0;i<data.length;i++){
+        //         promiseArr.push($http.get(serverUrl+'POI/'+data[i].poiID))             
+        //     }
+        //     Promise.all(promiseArr)
+        //     .then(function(result){
+        //         for(var i=0;i<result.length;i++){
+        //             var poi={};
+        //             poi=result[i].data.poidetails[0];
+        //             poi.image=result[i].data.images[0].image;
+        //             poi.reviews=result[i].reviews;
+        //             $scope.poiarray[poi.ID]=poi;
+        //             var Dbpois=dbpois.get_dbpois();
+        //             if(Dbpois){
+        //                 if(!$scope.checkIfExists(Dbpois,poi))
+        //                 dbpois.update_dbpois(poi);
+        //             }
+        //             else{
+        //                 dbpois.update_dbpois(poi);
+        //             }
+                   
+        //         }
+        //         localPoi=getlocalpois.get_local_pois();
+        //         if(localPoi){
+        //             for(var i=0;i<localPoi.length;i++){
+        //                 $scope.poiarray[localPoi[i].ID]=localPoi[i];
+        //             }
+        //         }
+        //         $scope.showimgs = true;
+        //         $scope.poiorder=Object.keys($scope.poiarray);
+        //         $scope.$apply();
+
+        //     })
+        // },function(response){
+        //     alert("something went wrong");
+        // })
         
         
         $scope.updatepostion_up = function(index){
