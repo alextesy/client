2cff13f27ac158f28a4da3c61dda3dd806b54213
Alexander Kremyanskyy <ALEXKREM@ibm.com>
2018-07-17 18:47:01 +0300

    updated home page
    2 last fixed (if there are no last saved)

--- components/homeCtrl.js
+++ components/homeCtrl.js
@@ -42,19 +42,25 @@ angular.module('poiApp')
             
     
         })
-
+        $scope.savedBool=[];
         $http.get(serverUrl+'users/log/2LastSaved')
         .then(function(result3){
+            if(result3.data.length===0){
+                $scope.savedBool=[false,false];
+                return;
+            }
             var lastSavedimg=[];
             var arrLastSaved=[];
-            for(var i=0;i<2;i++){
+            for(var i=0;i<result3.data.length;i++){
                 var urlSaved=serverUrl+'POI/'+result3.data[i].poiID;
+                $scope.savedBool.push(true);
                 arrLastSaved.push($http.get(urlSaved));
             }
             Promise.all(arrLastSaved)
             .then(function(result4){
-                lastSavedimg.push({"image":result4[0].data.images[0].image,"ID":result4[0].data.poidetails[0].ID,"name":result4[0].data.poidetails[0].name});
-                lastSavedimg.push({"image":result4[1].data.images[0].image,"ID":result4[1].data.poidetails[0].ID,"name":result4[1].data.poidetails[0].name});
+                for(var i=0;i<result4.length;i++){
+                    lastSavedimg.push({"image":result4[i].data.images[0].image,"ID":result4[i].data.poidetails[0].ID,"name":result4[i].data.poidetails[0].name});
+                }
                 $scope.lastSavedimg=lastSavedimg;
                 $scope.showimgs = true;
                 $scope.$apply();
